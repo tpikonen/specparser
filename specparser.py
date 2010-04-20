ed1dafdbfc7aead23acbae57a9d42ba7b1c77f9d
Teemu Ikonen <tpikonen@gmail.com>
2010-04-20 18:29:16 +0200

    Improve docstrings

--- specparser.py
+++ specparser.py
@@ -1,18 +1,21 @@
 import re, logging, time, datetime
 
+
 # Exceptions emitted by the parser
 class ParseError(Exception):
+    """Raised when the parser encounters a line which is really broken"""
     def __init__(self, line):
         self.line = line
     def __str__(self):
         return repr(self.line)
 
 class InputTimeout(Exception):
+    """Raised when input could not be read in self.timeout seconds"""
     pass
 
 class ScanEnd(Exception):
+    """Raised when end of scan is encountered when reading points"""
     pass
-
 
 def is_blankline(line):
     m = re.match('^\W*$', line)
@@ -127,7 +130,24 @@ class specparser:
 # Public methods
 
     def header(self):
-        """Returns the spec-file header (possibly incomplete) in a dictionary"""
+        """Returns the spec-file header in a dictionary.
+
+        The keys corresponding to spec-file header lines are
+
+        ======  =============== =====
+        SPEC    key             value
+        ======  =============== =====
+        #F      filename        string
+        #E      epoch           int, seconds since epoch
+        #D      date            date in datetime format
+        #On     motornames      list of strings
+        #x      unknown_headers list of [lineno, linestring] lists
+        ======  =============== =====
+
+        If the complete header is not written to the spec-file after waiting
+        :attr:`timeout` seconds, or if some header lines are missing,
+        then the header dictionary will be returned incomplete.
+        """
         self.state = self.in_header
         logging.debug("Parsing header")
         hdict = {}
@@ -174,9 +194,39 @@ class specparser:
         return hdict
 
 
+    def next_scan(self):
+        """Return a dictionary with the contents of the next scan in the
+        parsed file, or a Timeout exception.
+
+        In addition to the keys in the scan header
+        (see :method:`next_scan_header` for the scan header keys) the
+        following keys and values are present
+
+        ==============  ================
+        key             value
+        ==============  ================
+        points          list of point lists
+        point_comments  list of [pointnum, commentline] lists
+        ==============  ================
+
+        If the complete scan is not written to the spec-file after waiting
+        :attr:`timeout` seconds, then the scan dictionary will be returned
+        incomplete.
+        """
+        self.next_scan_header()
+        try:
+            while True:
+                self.next_point()
+        except ScanEnd:
+            pass
+        sdict = self.scanheader
+        sdict['points'] = self.points
+        return sdict
+
+
     def next_scan_header(self):
-        """Return a dictionary with the header of the next scan,
-        or InputTimeout"""
+        """Return a dictionary with the contents of the header of the
+        next scan in the parsed file, or InputTimeout"""
         self.state = self.in_scan_header
         cl = self._curline
         while cl[0:2] != '#S':
@@ -281,20 +331,6 @@ class specparser:
                     raise(ScanEnd)
         self.points.append(pts)
         return pts
-
-
-    def next_scan(self):
-        """Returns the next scan in the file (with all the points),
-        or Timeout exception"""
-        self.next_scan_header()
-        try:
-            while True:
-                self.next_point()
-        except ScanEnd:
-            pass
-        sdict = self.scanheader
-        sdict['points'] = self.points
-        return sdict
 
 
     def parse(self):
